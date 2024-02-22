c45925b2896f1ba30ff060c56607aa6af9c8fa2f
L-Semakale <l.semakale@alustudent.com>
2024-02-22 11:52:22 -0800

    updated file

--- backup-Negpod_24-q1/Negpod_24-q1/Select-emails.sh
+++ backup-Negpod_24-q1/Negpod_24-q1/Select-emails.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# Select emails from Students-list_1023.txt
-cut -d ',' -f 1 Students-list_1023.txt > student-emails.txt
-
+cut -d ',' -f 1 students-list_1023.txt > student-emails.txt
+echo "Emails extracted and saved to student-emails.txt"
